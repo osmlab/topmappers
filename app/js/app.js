7d480e6b6f0c95e75c4fa4e6bc055b112bf238f8
Rub21 <rub2106@gmail.com>
2013-03-29 18:03:49 -0500

    add

--- app/js/app.js
+++ app/js/app.js
@@ -2,39 +2,29 @@ var map_id = 'examples.map-4l7djmvo',
     features = [],
     interaction,
     map = mapbox.map('map');
-var recursos_search = [];
+
 
 
 map.addLayer(mapbox.layer().id(map_id));
 
-mm_data(mapData); 
 
+mm_data(mapData);
 
 map.centerzoom({
-    lat: -13.16048,
-    lon: -74.22565
-}, 15);
+    lat: 37.504,
+    lon: -94.668
+}, 5);
+
 map.setZoomRange(0, 18);
 
 function mapData(f) {
+    features = f[2].edicion;
+    //features = f;    
+    console.log(features);
 
-    features = f;
-    console.log('ultimopor aqui' + features);/*
-    
     markerLayer = mapbox.markers.layer().features(features);
-    markerLayer.factory(function(m) {
 
-        //var elem = mapbox.markers.simplestyle_factory(m);
-        var elem = simplestyle_factory_rub(m);
-        MM.addEvent(elem, 'click', function(e) {
-            map.ease.location({
-                lat: m.geometry.coordinates[1],
-                lon: m.geometry.coordinates[0]
-            }).zoom(map.zoom()).optimal();
-        });
 
-        return elem;
-    });
 
 
     interaction = mapbox.markers.interaction(markerLayer);
@@ -42,30 +32,7 @@ function mapData(f) {
     map.ui.zoomer.add();
     map.ui.zoombox.add();
     map.ui.hash.add();
-    interaction.formatter(function(feature) {
-        var o = '<h5 class="popover-geo-title">' + feature.nombre + '</h5>' +
-            '<p>' + feature.descripcion.substring(0, 100) + '...</p>' +
-            '<div class="well-toltip">' +
-            '<img style="height: 120px; width:120px;   margin-right: 3px;" src="' + feature.imagenes[0].url + '">' +
-            '<img style="height: 120px; width:120px;" src="' + feature.imagenes[1].url + '">' +
-            '</div>';
-        var a_button = '';
-        var a = feature.clase.replace(/\s/g, "");
 
-        if (a == 'RecursoTurístico') {
-            a_button = '<a  role="button" class="btn"  href="#detail" onclick="call_detaill_recurso(\'' + feature.idproducto + '\')"> Más Detalle</a>';
-
-        } else if (a == 'Hotel') {
-            a_button = '<a  role="button" class="btn"  href="#detail" onclick="call_detail_hotel(\'' + feature.idproducto + '\')"> Más Detalle</a>';
-
-        }
-        return o + a_button;
-    });
-    $('#map').removeClass('loading');
-
-
-    fill_search(features);
-    grid_images(features);*/
 }
 
 
--- app/js/get_data.js
+++ app/js/get_data.js
@@ -1,9 +1,8 @@
-
 function mm_data(callback) {
     if (typeof reqwest === 'undefined') {
         throw 'CSV: reqwest required for mm_recurso';
     }
-    var url = '../app/Slist.json';
+    var url = 'http://rub21.github.com/report_top_us/app/SList.json?callback=callback';
     reqwest({
         url: url,
         type: 'jsonp',
@@ -13,18 +12,30 @@ function mm_data(callback) {
     });
 
     function response(x) {
-        console.log(x);
-           var features = [];
-        for (var i = 0; i < x.length; i++) {
-            x[i]['properties'] = {};
-            x[i].properties['marker-size'] = 'large';
-            x[i].properties['marker-symbol'] = type_icon;
-            x[i].properties['marker-color'] = '#000000';
 
-            features.push(x[i]); 
-            consle.log(features);
+        var features = [];
 
-        }
+        for (var j = 0; j < x.length; j++) {
+
+            for (var i = 0; i < x[j].edicion.length; i++) {
+                x[j].edicion[i]['properties'] = {};
+                /*x[j].edicion[i].properties['marker-symbol'] = 'hotel';
+                x[j].edicion[i].properties['marker-size'] = 'small';
+                x[j].edicion[i].properties['marker-color'] = '#000000';*/
+                x[j].edicion[i].properties['title'] = 'Num changes:'+x[j].edicion[i].num_changes;            
+
+                x[j].edicion[i]['geometry'] = {};
+                x[j].edicion[i].geometry['type']= 'Point';
+                x[j].edicion[i].geometry['coordinates'] = [];                
+                x[j].edicion[i].geometry.coordinates[0] = x[j].edicion[i].lon;
+                x[j].edicion[i].geometry.coordinates[1] = x[j].edicion[i].lat;
+
+            }
+
+            features.push(x[i]);
+
+        };
+        //console.log(features);
 
 
         return callback(features);
