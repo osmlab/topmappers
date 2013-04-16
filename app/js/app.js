29b675b7d49ae3098692e68b13566bcb39868896
Rub21 <rub2106@gmail.com>
2013-04-16 12:44:11 -0500

    subtitle

--- app/js/app.js
+++ app/js/app.js
@@ -96,7 +96,8 @@ map.setZoomRange(0, 12);
 map.ui.zoomer.add();
 map.ui.zoombox.add();
 map.ui.hash.add();
-map.ui.attribution.add().content('<a href="http://www.openstreetmap.org/">(c) OpenStreetMap contributors</a>');
+map.ui.attribution.add()
+.content('<a href="http://www.openstreetmap.org/">(c) OpenStreetMap contributors</a>');
 
 
 mm_user(listUser);
